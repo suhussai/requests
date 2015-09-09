58cb7185b1273dc7e6cf89540287e320dd816c31
suhussai <suhussai@ualberta.ca>
2015-09-08 19:14:01 -0600

    fix for python2.6

--- requests/utils.py
+++ requests/utils.py
@@ -265,7 +265,7 @@ def dict_from_cookiejar(cj):
 
     :param cj: CookieJar object to extract cookies from.
     """
-    return {cookie.name:cookie.value for cookie in cj}
+    return dict((cookie.name, cookie.value) for cookie in cj)
 
 def add_dict_to_cookiejar(cj, cookie_dict):
     """Returns a CookieJar from a key/value dictionary.
